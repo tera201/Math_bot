833bef5fba008410ba744405fa2192eed10c2073
Roma <tera201@mail.ru>
2017-12-06 23:45:28 +0300

    +

---
+++ test3.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+from numpy import *
+class Grafics():
+    def __init__(self,string):
+        self.n = 10
+        self.f=string
+    def constructor(self):
+        lag = 0.01
+        #print(self.f)
+        x = arange(-self.n, self.n, lag)
+        try:
+            y = eval(self.f)
+        except Exception:
+            return True
+        figsize = (10, 10)
+        fig = plt.figure(figsize=figsize,dpi=70,)
+        plt.plot(x, y)
+        plt.title('F(X)')
+        plt.ylabel('Y')
+        plt.xlabel('X ')
+
+        plt.grid(True)
+
+        plt.savefig('pic.png')#, format='png')
+        #plt.show()
+#Grafics('x**3').constructor()
